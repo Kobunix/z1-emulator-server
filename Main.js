8864992dc11d348ad9fb53f0524dd94d5ffff841
Kobunix <kobunix@gmail.com>
2020-10-29 05:37:26 +0100

    Update Main.js

---
+++ Main.js
@@ -0,0 +1,19 @@
+var Functions = require('./Core/Functions.js');
+const http = require('http');
+
+
+/* Variables */
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+// HTTP SERVER - TO EMULATE EVERY SOE SERVERS - Need opti
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
