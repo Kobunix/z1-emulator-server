75a259e51670eb54cf64c2f1c9b85173cdabd7bb
Kobunix <kobunix@gmail.com>
2020-10-29 07:16:20 +0100

    Formatting App

--- Main.js
+++ Main.js
@@ -2,16 +2,33 @@
 					H1Z1 (JUST SURVIVE) EMULATOR - ALPHA VERSION
  *********************************************************************************/
 
+
 /* ALL NEEDED REQUIRES */
 // const args = require('minimist')(process.argv.slice(2));
 var colors = require('colors/safe');
 
-require('./Core/Functions');
+/* Included Scripts */
+var func = require('./Core/Functions');
 require('./Core/FakeServers');
 
+	// NOTES : 
+		// fservers.CreateServer("LoginServer","127.0.0.1", "20042");
+		// CreateServer("GateWayServer","127.0.0.1", "20043");
 
-// fservers.CreateServer("LoginServer","127.0.0.1", "20042");
-// CreateServer("GateWayServer","127.0.0.1", "20043");
-/* Variables */
 
+/* APP START */
+function InitEmu()
+{
+	// Make here all logic
+	console.log(" ");
+	console.log(colors.rainbow('=================================================================='));
+	console.log(colors.white('		  H1Z1 (JUST SURVIVE) EMULATOR'));
+	console.log(colors.rainbow('=================================================================='));
+	console.log(" ");
 
+	func.ReturnConsoleCommand("testlab","textcontent");
+	func.test();
+
+}
+
+InitEmu();
