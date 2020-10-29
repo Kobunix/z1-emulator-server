7b66a099103527f79aa77de7ed21188c21b72f45
Kobunix <kobunix@gmail.com>
2020-10-29 06:35:25 +0100

    Updated Core

--- Main.js
+++ Main.js
@@ -1,7 +1,11 @@
+/*********************************************************************************
+					H1Z1 (JUST SURVIVE) EMULATOR - ALPHA VERSION
+ *********************************************************************************/
+
+/* ALL NEEDED REQUIRES */
+const args = require('minimist')(process.argv.slice(2));
 var Functions = require('./Core/Functions.js');
-const FServers = require('./Core/FakeServers.js');
-
-
+var FServers = require('./Core/FakeServers.js');
 
 
 /* Variables */
